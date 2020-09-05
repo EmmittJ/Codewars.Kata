45f187b720577f74f0200ccd0e30f7ec233cd749
Emmitt <1337ERJ1337@gmail.com>
2020-09-05 13:25:58 -0700

    Completed: https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/csharp

---
+++ Codewars/4kyu/SumOfIntervals/SumOfIntervals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Codewars._4kyu.SumOfIntervals
+{
+    public class Intervals
+    {
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            intervals = intervals.OrderBy(x => x.Item1).ToArray();
+            var current = (0, 0);
+            var sum = 0;
+            foreach (var interval in intervals)
+            {
+                if ((interval.Item1 >= current.Item1 && interval.Item1 <= current.Item2) || (interval.Item1 <= current.Item1 && interval.Item2 >= current.Item1))
+                {
+                    current.Item2 = Math.Max(current.Item2, interval.Item2);
+                    current.Item1 = Math.Min(current.Item1, interval.Item1);
+                }
+                else
+                {
+                    sum += (current.Item2 - current.Item1);
+                    current = interval;
+                }
+
+            }
+
+            sum += (current.Item2 - current.Item1);
+            return sum;
+        }
+    }
+}
